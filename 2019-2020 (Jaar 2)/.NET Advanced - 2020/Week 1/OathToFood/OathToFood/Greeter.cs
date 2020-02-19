6b0fce7d75a30344326a6ca1f1179816fe6999e3
HungryDev <algorithm1401@gmail.com>
2020-02-19 12:14:53 +0100

    .NET Essentials - OathFood application for introduction to TDD (Test driven development)

---
+++ 2019-2020 (Jaar 2)/.NET Advanced - 2020/Week 1/OathToFood/OathToFood/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OathToFood
+{
+    public class Greeter : IGreeter
+    {
+        public string GetMessageOfTheDay()
+        {
+            return "Stop deze kutles aub";
+        }
+    }
+}
